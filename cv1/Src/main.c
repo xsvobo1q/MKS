5829de00c8f5c09107458dcd3d3dd11438ea6853
Marek Svoboda <marsvobo1@seznam.cz>
2021-09-23 11:18:46 +0200

    cviko jedna

--- cv1/Src/main.c
+++ cv1/Src/main.c
@@ -18,6 +18,7 @@
  */
 
 #include <stdint.h>
+#include "stm32f0xx.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -25,6 +26,12 @@
 
 int main(void)
 {
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+
     /* Loop forever */
-	for(;;);
+	while(1){
+		GPIOA->ODR ^= (1<<5); // toggle
+		for (volatile uint32_t i = 0; i < 100000; i++) {}
+	}
 }
