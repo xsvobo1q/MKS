c489e80d845802209a7fd40372ceeb4ad64c4e3e
Simona Sadlkeova <174528@vut.cz>
2021-11-04 12:42:10 +0100

    Vyreseno...

--- cv7/Core/Src/main.c
+++ cv7/Core/Src/main.c
@@ -24,6 +24,8 @@
 /* USER CODE BEGIN Includes */
 #include "1wire.h"
 #include "sct.h"
+#include "lookup.h"
+#include <stdbool.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -33,6 +35,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -41,6 +44,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc;
+
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
@@ -51,12 +56,36 @@ UART_HandleTypeDef huart2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_ADC_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+bool buttons(void){
+	static bool stateButton;
+	static uint32_t old_s2;
+	static uint32_t old_s1;
+	static uint32_t delay;
+	uint32_t new_s2 = HAL_GPIO_ReadPin(S2_GPIO_Port,S2_Pin);
+	uint32_t new_s1 = HAL_GPIO_ReadPin(S1_GPIO_Port,S1_Pin);
+
+	if (HAL_GetTick() > delay + 40){
+		if (old_s2 && !new_s2) { // falling edge
+			stateButton = true;
+		}
+		if (old_s1 && !new_s1){
+			stateButton = false;
+		}
+		old_s2 = new_s2;
+		old_s1 = new_s1;
+
+		delay = HAL_GetTick();
+	}
+	return stateButton;
+}
 
 /* USER CODE END 0 */
 
@@ -76,8 +105,7 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-  OWInit();
-  sct_init();
+
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -90,8 +118,12 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_ADC_Init();
   /* USER CODE BEGIN 2 */
-
+  OWInit();
+  sct_init();
+  HAL_ADCEx_Calibration_Start(&hadc);
+  HAL_ADC_Start(&hadc);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -101,12 +133,28 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	OWConvertAll();
-	HAL_Delay(CONVERT_T_DELAY);
-	int16_t temp_18b20;
-	OWReadTemperature(&temp_18b20);
-	temp_18b20 = temp_18b20/10;
-	sct_value(temp_18b20, 0);
+
+	static int16_t temp_18b20;
+	static uint32_t convertDelay;
+	bool button = buttons();
+
+	if (button){
+		int16_t tempNTC = lookup[HAL_ADC_GetValue(&hadc)];
+		sct_value(tempNTC, 0);
+		HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 1);
+		HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 0);
+	}else
+	{
+		HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 0);
+		HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 1);
+		if (HAL_GetTick() > convertDelay + CONVERT_T_DELAY){
+			OWReadTemperature(&temp_18b20);
+			temp_18b20 = temp_18b20 / 10;
+			sct_value(temp_18b20, 0);
+			OWConvertAll();
+			convertDelay = HAL_GetTick();
+		}
+	}
 
   }
   /* USER CODE END 3 */
@@ -124,9 +172,11 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSI14;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSI14State = RCC_HSI14_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.HSI14CalibrationValue = 16;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
@@ -147,6 +197,58 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC_Init(void)
+{
+
+  /* USER CODE BEGIN ADC_Init 0 */
+
+  /* USER CODE END ADC_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC_Init 1 */
+
+  /* USER CODE END ADC_Init 1 */
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc.Instance = ADC1;
+  hadc.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc.Init.Resolution = ADC_RESOLUTION_10B;
+  hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc.Init.ScanConvMode = ADC_SCAN_DIRECTION_FORWARD;
+  hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc.Init.LowPowerAutoWait = DISABLE;
+  hadc.Init.LowPowerAutoPowerOff = DISABLE;
+  hadc.Init.ContinuousConvMode = ENABLE;
+  hadc.Init.DiscontinuousConvMode = DISABLE;
+  hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc.Init.DMAContinuousRequests = DISABLE;
+  hadc.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
+  if (HAL_ADC_Init(&hadc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure for the selected ADC regular channel to be converted.
+  */
+  sConfig.Channel = ADC_CHANNEL_1;
+  sConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
+  sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;
+  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC_Init 2 */
+
+  /* USER CODE END ADC_Init 2 */
+
 }
 
 /**
